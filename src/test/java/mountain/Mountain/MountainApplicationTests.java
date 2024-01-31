bb8cbfd9ba93f51ff476602b3f5d2720bb72289f
HanheeJang <jangjang402@gmail.com>
2024-01-31 21:48:57 +0900

    Initial commit

---
+++ src/test/java/mountain/Mountain/MountainApplicationTests.java
@@ -0,0 +1,13 @@
+package mountain.Mountain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
