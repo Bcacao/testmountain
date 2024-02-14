2fa568e2fd05276b87b795ffbdce9f9fe205382b
HanheeJang <jangjang402@gmail.com>
2024-02-14 20:25:29 +0900

    Commit - 필요없는 환경 설정 파일 삭제 및 Controller에 Test code 추가

--- src/main/java/mountain/Mountain/Controller/MainController.java
+++ src/main/java/mountain/Mountain/Controller/MainController.java
@@ -1,5 +1,14 @@
 package mountain.Mountain.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
-    String test = "test";
+
+    @RequestMapping("home")
+    public String home(){
+        return "home";
+    }
 }
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,12 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>홈</title>
-</head>
-<body>
-홈
-</body>
 
-</html>
+<script>
+    location.href="home";
+</script>
